6f87452eaccff378607528d18d76b1a925ea647d
Rasheed Mozaffar <rasheedkmozaffar@hotmail.com>
2024-06-24 18:19:08 +0300

    Moved get post by id query tests to a separate file

--- WebBlogger.Tests/Application/Queries/Posts/GetLatestPostsQueryTests.cs
+++ WebBlogger.Tests/Application/Queries/Posts/GetLatestPostsQueryTests.cs
@@ -4,11 +4,8 @@ using Application.Common.Interfaces;
 using Application.Common.Responses;
 using Application.Features.Posts.Handlers;
 using Application.Features.Posts.Queries;
-using Domain.Enums;
-using Domain.Exceptions;
 using Infrastructure.Repositories;
 using Microsoft.Extensions.Logging;
-using NSubstitute.ExceptionExtensions;
 
 namespace WebBlogger.Tests.Application.Queries.Posts;
 
@@ -45,59 +42,5 @@ public class GetLatestPostsQueryTests
 
         // ASSERT
         await action.Should().ThrowAsync<OperationCanceledException>();
-    }
-}
-
-public class GetPostByIdQueryTests
-{
-    private readonly IPostsRepository _postsRepository;
-
-    public GetPostByIdQueryTests()
-    {
-        _postsRepository = Substitute.For<IPostsRepository>();
-    }
-
-    [Fact]
-    public async Task Handle_ShouldThrowPostNotFoundException_WhenGivenPostIdDoesNotExist()
-    {
-        // ARRANGE
-        var query = new GetPostByIdQuery(Guid.NewGuid()); // Random ID
-        var handler = new GetPostByIdQueryHandler(_postsRepository);
-
-        _postsRepository.GetPostByIdAsync(Arg.Any<Guid>(), Arg.Any<CancellationToken>())
-            .Throws(new PostNotFoundException("Post was not found"));
-        // ACT
-        var action = async () => await handler.Handle(query, CancellationToken.None);
-
-        // ASSERT
-        await action.Should().ThrowAsync<PostNotFoundException>()
-            .WithMessage("Post was not found");
-    }
-
-    [Fact]
-    public async Task Handle_ShouldReturnTheCorrectPost_WhenTheGivenPostIdExists()
-    {
-        // ARRANGE
-        var postId = Guid.NewGuid();
-        var query = new GetPostByIdQuery(postId);
-        var handler = new GetPostByIdQueryHandler(_postsRepository);
-
-        var expectedPost = new Post()
-        {
-            Id = postId,
-            Title = "Hello, World",
-            Content = "Hello, from the actual world!",
-            Status = PostStatus.Published
-        };
-
-        _postsRepository.GetPostByIdAsync(postId, Arg.Any<CancellationToken>())
-            .Returns(expectedPost);
-
-        // ACT
-        var result = await handler.Handle(query, CancellationToken.None);
-
-        // ASSERT
-        result.Should().NotBeNull();
-        await _postsRepository.Received(1).GetPostByIdAsync(postId, Arg.Any<CancellationToken>());
     }
 }
