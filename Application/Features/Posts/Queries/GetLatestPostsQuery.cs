5177972390f245bd0e1fc3e561181ced06366cdf
Rasheed Mozaffar <rasheedkmozaffar@hotmail.com>
2024-06-24 15:39:32 +0300

    Added missing sort option property

--- Application/Features/Posts/Queries/GetLatestPostsQuery.cs
+++ Application/Features/Posts/Queries/GetLatestPostsQuery.cs
@@ -1,13 +1,17 @@
+using Application.Common.Enums;
+using Application.Common.Responses;
 using Domain.Models;
 using MediatR;
 
 namespace Application.Features.Posts.Queries;
 
-public class GetLatestPostsQuery(int pageNumber, int pageSize, string? title = null) : IRequest<IReadOnlyCollection<Post>>
+public class GetLatestPostsQuery(int pageNumber, int pageSize, PostSortOption sortOption, string? title = null) : IRequest<PaginatedReadOnlyCollection<Post>>
 {
     public int PageNumber { get; } = pageNumber;
 
     public int PageSize { get; } = pageSize;
 
     public string? Title { get; } = title;
+
+    public PostSortOption SortOption { get; } = sortOption;
 }
