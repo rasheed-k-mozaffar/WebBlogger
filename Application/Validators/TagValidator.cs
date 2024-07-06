7d22a5d3d102b5c30610712de4ba0b45ec8e7009
Rasheed Mozaffar <rasheedkmozaffar@hotmail.com>
2024-07-06 18:56:05 +0300

    Added missing check for null name

--- Application/Validators/TagValidator.cs
+++ Application/Validators/TagValidator.cs
@@ -8,10 +8,11 @@ public class TagValidator : AbstractValidator<Tag>
     public TagValidator()
     {
         RuleFor(p => p.Name)
-        .NotEmpty().WithMessage("The tag name cannot be empty.")
-        .MaximumLength(100).WithMessage("The tag name cannot be longer than 100 characters.");
+            .NotNull().WithMessage("Name is required.")
+            .NotEmpty().WithMessage("The tag name cannot be empty.")
+            .MaximumLength(100).WithMessage("The tag name cannot be longer than 100 characters.");
 
         RuleFor(p => p.Description)
-        .MaximumLength(2000).WithMessage("Tag description cannot be longer than 2000 characters.");
+            .MaximumLength(2000).WithMessage("Tag description cannot be longer than 2000 characters.");
     }
 }
