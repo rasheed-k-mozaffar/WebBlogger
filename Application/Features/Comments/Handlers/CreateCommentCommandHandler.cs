fbc7e405fc9c78c7980d81cf55679fa63b90cb3f
Rasheed Mozaffar <rasheedkmozaffar@hotmail.com>
2024-07-19 09:26:55 +0300

    Finished the CreateComment command handler and its tests

---
+++ Application/Features/Comments/Handlers/CreateCommentCommandHandler.cs
@@ -0,0 +1,36 @@
+using Application.Common.Interfaces;
+using Application.Common.Mappers;
+using Application.Features.Comments.Commands;
+using Domain.Models;
+using FluentValidation;
+using MediatR;
+
+namespace Application.Features.Comments.Handlers;
+
+public class CreateCommentCommandHandler
+    (
+        ICommentsRepository commentsRepository,
+        IValidator<Comment> validator
+    )
+    : IRequestHandler<CreateCommentCommand, Comment>
+{
+    public async Task<Comment> Handle(CreateCommentCommand request, CancellationToken cancellationToken)
+    {
+        var comment = request.ToComment();
+        var validationResult = await validator.ValidateAsync(comment, cancellationToken);
+
+        if (!validationResult.IsValid)
+            throw new ValidationException(validationResult.Errors);
+
+        var result = await commentsRepository
+            .SaveCommentAsync
+            (
+                request.PostId,
+                comment,
+                comment.ParentCommentId,
+                cancellationToken
+            );
+
+        return result;
+    }
+}
---
+++ WebBlogger.Tests/Application/Commands/Comments/CreateCommentCommandHandlerTests.cs
@@ -0,0 +1,138 @@
+using System.Linq.Expressions;
+using Application.Common.Interfaces;
+using Application.Features.Comments.Commands;
+using Application.Features.Comments.Handlers;
+using FluentValidation;
+using FluentValidation.Results;
+using NSubstitute.ExceptionExtensions;
+
+namespace WebBlogger.Tests.Application.Commands.Comments;
+
+public class CreateCommentCommandHandlerTests
+{
+    private Guid _postId;
+    private Guid _commentId;
+
+    private readonly IValidator<Comment> _validator;
+    private readonly ICommentsRepository _commentsRepository;
+    private readonly CreateCommentCommandHandler _handler;
+
+    public CreateCommentCommandHandlerTests()
+    {
+        _postId = Guid.NewGuid();
+        _commentId = Guid.NewGuid();
+
+        _validator = Substitute.For<IValidator<Comment>>();
+        _commentsRepository = Substitute.For<ICommentsRepository>();
+        _handler = new CreateCommentCommandHandler(_commentsRepository, _validator);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldSaveCommentAsync_WhenCommandIsValid()
+    {
+        // ARRANGE
+        var comment = new Comment()
+        {
+            Id = _commentId,
+            PostId = _postId,
+            Content = "Valid Comment Content"
+        };
+
+        var command = new CreateCommentCommand
+        (
+            comment.Id,
+            comment.PostId,
+            null,
+            Guid.NewGuid(),
+            comment.Content
+        );
+
+        _validator.ValidateAsync(Arg.Any<Comment>(), Arg.Any<CancellationToken>())
+            .Returns(new ValidationResult());
+
+        _commentsRepository.SaveCommentAsync
+        (
+            Arg.Any<Guid>(),
+            Arg.Any<Comment>(),
+            null,
+            Arg.Any<CancellationToken>()
+        ).Returns(comment);
+
+        // ACT
+        var result = await _handler.Handle(command, CancellationToken.None);
+
+        // ASSERT
+        result.Should().NotBeNull();
+        result.Id.Should().Be(_commentId);
+        result.PostId.Should().Be(_postId);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldThrowValidationException_WhenCommandIsInvalid()
+    {
+        // ARRANGE
+        var command = new CreateCommentCommand
+        (
+            _commentId,
+            _postId,
+            null,
+            Guid.NewGuid(),
+            string.Empty
+        );
+
+        List<ValidationFailure> failures =
+        [
+            new ValidationFailure
+            (
+                nameof(Comment.Content),
+                "The comment cannot be empty"
+            )
+        ];
+
+        Expression<Func<ValidationException, bool>> contentValidationErrorHasOccured =
+            (ValidationException e) =>
+            e.Errors.Any(error => error.PropertyName == nameof(Comment.Content));
+
+        _validator.ValidateAsync(Arg.Any<Comment>(), Arg.Any<CancellationToken>())
+            .ThrowsAsync(new ValidationException(failures));
+
+        Func<Task> action = async() => await _handler.Handle(command, CancellationToken.None);
+
+        // ACT & ASSERT
+        await action.Should().ThrowAsync<ValidationException>();
+        await action.Should().ThrowAsync<ValidationException>()
+            .Where(contentValidationErrorHasOccured);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldThrowOperationCancelledException_WhenCancellationIsRequested()
+    {
+        // ARRANGE
+        var command = new CreateCommentCommand
+        (
+            _commentId,
+            _postId,
+            null,
+            Guid.NewGuid(),
+            "Test comment"
+        );
+        var cts = new CancellationTokenSource();
+
+        _validator.ValidateAsync(Arg.Any<Comment>(), Arg.Any<CancellationToken>())
+            .Returns(new ValidationResult());
+
+        _commentsRepository.SaveCommentAsync
+        (
+            Arg.Any<Guid>(),
+            Arg.Any<Comment>(),
+            null,
+            Arg.Any<CancellationToken>()
+        ).ThrowsAsync(new OperationCanceledException());
+
+        await cts.CancelAsync();
+        Func<Task> action = async () => await _handler.Handle(command, cts.Token);
+
+        // ACT & ASSERT
+        await action.Should().ThrowAsync<OperationCanceledException>();
+    }
+}
